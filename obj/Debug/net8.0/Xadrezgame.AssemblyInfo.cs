13f71ab0b8e18b26a1de9fa89bcfea1913926973
Pedro Sales <pedrosalesdev@gmail.com>
2024-06-11 09:01:06 -0300

    Implemetando a posição

--- obj/Debug/net8.0/Xadrezgame.AssemblyInfo.cs
+++ obj/Debug/net8.0/Xadrezgame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xadrezgame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b855f333ede57343d752fc434055225ec73adcd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4689a0f871de0ce4e2212814512d8d71e0b6f20d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xadrezgame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xadrezgame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
